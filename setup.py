8c373b0a1b2f6e0208b7969ae8f57b3bc57352b3
David Ahijevych <ahijevyc@ucar.edu>
2024-12-04 11:09:43 -0700

    Update and rename index.html to README.md
    not cm1-web

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(name="cm1",
       version="0.1",
       author="David Ahijevych",
       license="MIT",
-      url="https://github.com/ahijevyc/cm1-web",
+      url="https://github.com/ahijevyc/cm1",
       packages=["cm1"],
       install_requires=requirements)
