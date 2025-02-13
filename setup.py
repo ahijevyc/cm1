6361ee29e8c25cef4778c3b69ffd725cc2ec960c
David Ahijevych <ahijevyc@ucar.edu>
2025-02-12 17:13:58 -0700

    Fix author
    add dependencies

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ requirements = []
 
 setup(name="cm1",
       version="0.1",
-      author="David Ahijevych",
+      author="George Bryan",
       license="MIT",
       url="https://github.com/ahijevyc/cm1",
       packages=["cm1"],
